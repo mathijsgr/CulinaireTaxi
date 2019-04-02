8d152dac7e86c0bc437d06ab8706b650712bf570
Damiaen <dtoussaint97@gmail.com>
2019-04-02 21:23:14 +0200

    Add project files.

---
+++ CulinaireTaxi/App_Code/Gift.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Todo
+/// </summary>
+public class Gift
+{
+    public int GiftId { get; set; }
+    public string GiftName { get; set; }
+    public string GiftPrice { get; set; }
+    public string HasBeenBoughtBy { get; set; }
+    public string Location { get; set; }
+    public string Description { get; set; }
+    public int Priority { get; set; }
+}
---
+++ CulinaireTaxi/App_Code/GiftList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Todo
+/// </summary>
+public class GiftList
+{
+    public int ListId { get; set; }
+    public int OwnerId { get; set; }
+    public string ListCode { get; set; }
+    public string ListName { get; set; }
+    public DateTime LastEdited { get; set; }
+}
